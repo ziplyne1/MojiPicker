7539022e8794aa9e41063c93fad077cb0d296ade
Pax Willoughby <140573952+potatoDOTzip@users.noreply.github.com>
2026-04-10 21:22:59 -0700

    Add models

---
+++ MojiPicker/Sources/MojiPicker/Sources/Models/Emoji.swift
@@ -0,0 +1,7 @@
+struct Emoji {
+    let symbol: String
+    let description: String
+    let category: EmojiCategory
+    let aliases: [String]
+    let tags: [String]
+}
---
+++ MojiPicker/Sources/MojiPicker/Sources/Models/EmojiCategory.swift
@@ -0,0 +1,11 @@
+enum EmojiCategory: String {
+    case smileys = "Smileys & Emotion"
+    case people = "People & Body"
+    case nature = "Animals & Nature"
+    case food = "Food & Drink"
+    case travel = "Travel & Places"
+    case activities = "Activities"
+    case objects = "Objects"
+    case symbols = "Symbols"
+    case flags = "Flags"
+}
