f12fea28910d7d07c63acf2bd5b51080da8c572d
Pax Willoughby <140573952+potatoDOTzip@users.noreply.github.com>
2026-04-10 20:48:53 -0700

    Initial commit Add the skeleton

---
+++ MojiPicker/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MojiPicker",
+    platforms: [
+        .iOS(.v26)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MojiPicker",
+            targets: ["MojiPicker"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MojiPicker"
+        ),
+        .testTarget(
+            name: "MojiPickerTests",
+            dependencies: ["MojiPicker"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
