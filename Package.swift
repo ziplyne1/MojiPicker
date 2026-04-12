d8a17775f1c268be304ddfa2e77311c07c449047
Pax Willoughby <140573952+potatoDOTzip@users.noreply.github.com>
2026-04-11 22:46:45 -0700

    Remove macOS as a target

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MojiPicker",
     platforms: [
-        .iOS(.v26),
-        .macOS(.v26)
+        .iOS(.v26)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
