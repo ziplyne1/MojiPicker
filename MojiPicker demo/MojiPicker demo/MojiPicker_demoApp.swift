b79acb9a459dba2ccd9d377b9d6d51ee1f7a6629
Pax Willoughby <140573952+potatoDOTzip@users.noreply.github.com>
2026-04-11 23:51:20 -0700

    Initial commit for demo app

---
+++ MojiPicker demo/MojiPicker demo/MojiPicker_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiPicker_demoApp.swift
+//  MojiPicker demo
+//
+//  Created by Pax Willoughby on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MojiPicker_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
